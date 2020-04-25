ec3c74cc077c65eeb58ed7bf2ed03a4173b1e31a
Yair Dar <zvulon+fork.yairv.dar@gmail.com>
2020-04-25 23:17:10 +0300

    pydeps:tut: freeze step_01_dirty_test_readme

--- tutorials/pyprjs/01-app-pydeps/step_01_dirty_test_readme/pydeps/pydeps.py
+++ tutorials/pyprjs/01-app-pydeps/step_01_dirty_test_readme/pydeps/pydeps.py
@@ -7,12 +7,27 @@ def analize():
 
 
 def main():
-    print(f"Hello from pydeps to {getpass.getuser()}")
     print("Analizing Interpreter and Virtual Environment")
     import pprint
-    res = analize()
-    print(pprint.pformat(res))
+    report = analize()
+    print(pprint.pformat(report))
+    print(f"Loaded Modules Count: {len(report['modules.loaded.list'])}")
+    print(f"Hello from pydeps to {getpass.getuser()}")
+
+
+def test():
+    print("=== test:test started ===")
+    report = analize()
+    loaded = report['modules.loaded.list']
+    assert 'getpass' in loaded, \
+        f'Missing module getpass. Not found in list of {len(loaded)} modules'
+    print(f"Success found getpass in list of {len(loaded)} modules")
+    print("=== test:test passed ===")
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+    if '--test' in sys.argv:
+        test()
+    else:
+        main()
