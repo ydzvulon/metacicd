7ab2ac8cac362ba2f622cff9a6ca71ed9d3f7691
Yair Dar <zvulon+fork.yairv.dar@gmail.com>
2020-04-26 02:33:01 +0300

    pydeps:tut: sync to step step_03_venv_pytest

--- tutorials/pyprjs/01-app-pydeps/step_03_venv_pytest/pydeps/pydeps.py
+++ tutorials/pyprjs/01-app-pydeps/step_03_venv_pytest/pydeps/pydeps.py
@@ -1,9 +1,33 @@
+import sys
 import getpass
+import subprocess
 
 
 def analize():
-    import sys
-    return {'modules.loaded.list': set(sys.modules.keys())}
+    report = {}
+    report.update(analize_loaded())
+    report.update(analize_pip_list())
+    report.update(analize_pip_freeze())
+    return report
+
+
+def analize_loaded():
+    loaded = set(it for it in sys.modules.keys() if not it.startswith('_'))
+    return {'modules.loaded.list': loaded}
+
+
+def analize_pip_list():
+    out = subprocess.check_output(
+        f'{sys.executable} -m pip list', 
+        shell=True).decode()
+    return {'modules.pip-list.list': out}
+
+
+def analize_pip_freeze():
+    out = subprocess.check_output(
+        f'{sys.executable} -m pip freeze', 
+        shell=True).decode()
+    return {'modules.pip-freeze.list': out}
 
 
 def main():
