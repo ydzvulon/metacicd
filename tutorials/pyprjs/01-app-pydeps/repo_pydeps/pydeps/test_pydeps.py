3bd0425c55f67f15896cada1466be5e9abc6d44b
Yair Dar <zvulon+fork.yairv.dar@gmail.com>
2020-04-26 02:47:00 +0300

    pydeps: add flow asserts to test

--- tutorials/pyprjs/01-app-pydeps/repo_pydeps/pydeps/test_pydeps.py
+++ tutorials/pyprjs/01-app-pydeps/repo_pydeps/pydeps/test_pydeps.py
@@ -1,10 +1,20 @@
 import pydeps
 
-def test__pydeps__analize():
-    print("=== test:test started ===")
-    report = pydeps.analize()
+
+def assert_getpass_is_loaded(report):
     loaded = report['modules.loaded.list']
     assert 'getpass' in loaded, \
         f'Missing module getpass. Not found in list of {len(loaded)} modules'
     print(f"Success found getpass in list of {len(loaded)} modules")
-    print("=== test:test passed ===")
+
+
+def assert_pip_report(report):
+    assert report['modules.pip-list.list']
+    assert report['modules.pip-freeze.list']
+
+
+def test__pydeps__analize():
+    report = pydeps.analize()
+    assert_getpass_is_loaded(report)
+    assert_pip_report(report)
+
