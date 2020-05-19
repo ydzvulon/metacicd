d69b54455899af81143b2344800a44e351eb8e1f
Yair Dar <zvulon+fork.yairv.dar@gmail.com>
2020-05-19 22:51:26 +0300

    jenlib: add build.groovy - jenkinsfiles uses basic jenlib

---
+++ tutorials/jenlib/01-jenlib/parallel_tasks_jen/build.groovy
@@ -0,0 +1,39 @@
+@Library("jenlib@master") _
+// ===== /SHARED PART ====
+
+// --- globas vars ---
+def jg = [
+    scmvars: null,
+    cmds: [],
+    tstages: [:],
+    prj_dir: '.',
+    commits: [],
+    params: [
+        samle: 'yes'
+    ]
+]
+
+node() {timestamps {
+
+    stage('fetch'){
+        echo 'Fetch source'
+        def scmvars = checkout scm
+        def more = "repo_validate"
+        jen.set_build_name(currentBuild, scmvars, more)
+        jg['scmvars'] = scmvars
+    }
+
+    stage('commits'){
+        jen.desc_from_commits(currentBuild, jg)
+    }
+
+    catchError { dir('tutorials/jenlib/01-jenlib/parallel_tasks_jen'){
+        jen.step_stages_from_tasks(
+            jg, jg.prj_dir,
+            'Taskfile.yml', 'ci-flow')
+    }}
+
+    stage('finish'){
+        echo "done"
+    }
+}}
